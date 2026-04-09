0e45f15ea5953ef1d1c7ff98c3bf232fe8248703
AnjaliGhildiyal5 <ghildiyalanjali1985@gmail.com>
2026-04-09 23:44:32 +0530

    day 1

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b; // Uses the + operator to add numbers
+        
+        System.out.println("Sum = " + sum);
+    }
+}
